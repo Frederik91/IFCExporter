3d987457a9fd4ef42ac7891afc80fdc34e69d824
Frederik Tegnander <frederik.t91@gmail.com>
2015-11-09 15:41:59 +0100

    Påbegynt FolderMonitor

---
+++ IFCExporter/Models/FolderDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFCExporter.Models
+{
+    class FolderDate
+    {
+        public string Path { get; set; }
+        public DateTime LastUpdatet { get; set; }
+        public string Export { get; set; }
+        public string Discipline { get; set; }
+    }
+}
---
+++ IFCExporter/Workers/FolderMonitor.cs
@@ -0,0 +1,45 @@
+﻿using IFCExporter.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFCExporter.Workers
+{
+    public class FolderMonitor
+    {
+        private List<FolderDate> MonitorFolders(List<Discipline> Disciplines)
+        {
+            var FolderDateList = new List<FolderDate>();
+
+            foreach (var Discipline in Disciplines)
+            {
+                foreach (var Export in Discipline.Exports)
+                {
+                    foreach (var Folder in Export.Folders)
+                    {
+                        FolderDateList.Add(new FolderDate { Path = Folder.From, LastUpdatet = Directory.GetLastWriteTime(Folder.From), Discipline = Discipline.Name, Export = Export.Name});
+                    }                    
+                }
+            }
+            return FolderDateList;
+        }
+
+        private void CheckIfFolderIsUpdated(List<FolderDate> NewFolderList, List<FolderDate> OldFolderList)
+        {
+            foreach (var oldFolder in OldFolderList)
+            {
+                foreach (var newFolder in NewFolderList)
+                {
+                    if (newFolder.Path == oldFolder.Path)
+                    {
+
+                    }
+                }
+            }
+        }
+
+    }
+}
