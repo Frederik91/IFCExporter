5d82106cf4030b4671a0edca6da8cb7d1f0b4a28
Frederik Tegnander <frederik.t91@gmail.com>
2016-02-01 12:03:03 +0100

    Satt hele Exportall.run() i en try-catch for debugging.

--- IFCExporter/Workers/ExportAll.cs
+++ IFCExporter/Workers/ExportAll.cs
@@ -29,104 +29,113 @@ namespace IFCExporter.Models
 
         public void Run()
         {
-            var OAC = new DrawingManager();
-            var Exports = DataStorage.ExportsToRun;
-            var Disciplines = DataStorage.ProjectInfo.Disciplines;
 
-            foreach (var Discipline in Disciplines)
+            try
             {
-                foreach (var Export in Discipline.Exports)
+                var OAC = new DrawingManager();
+                var Exports = DataStorage.ExportsToRun;
+                var Disciplines = DataStorage.ProjectInfo.Disciplines;
+
+                foreach (var Discipline in Disciplines)
                 {
-                    foreach (var exp in Exports)
+                    foreach (var Export in Discipline.Exports)
                     {
-                        if (exp == Export.Name)
+                        foreach (var exp in Exports)
                         {
-                            var text = new List<string>();
-                            text.Add("Running export: " + Export.Name + "\n");
-                            File.AppendAllLines("c:\\IFCEXPORT\\log.txt", text);
-
-                            foreach (var Folder in Export.Folders)
+                            if (exp == Export.Name)
                             {
-                                //Last ned mappe med modellfiler
+                                var text = new List<string>();
+                                text.Add("Running export: " + Export.Name + "\n");
+                                File.AppendAllLines("c:\\IFCEXPORT\\log.txt", text);
 
-                                UnloadAllXrefs UAX = new UnloadAllXrefs();
-                                if (AutomaticBool)
+                                foreach (var Folder in Export.Folders)
                                 {
-                                    var FilesWithChanges = DataStorage.FilesWithChanges;
-                                    var FilesToUnload = new List<string>();
+                                    //Last ned mappe med modellfiler
 
-                                    foreach (var _file in FilesWithChanges)
+                                    UnloadAllXrefs UAX = new UnloadAllXrefs();
+                                    if (AutomaticBool)
                                     {
-                                        var ToPath = DataStorage.ProjectInfo.BaseFolder.To + _file.Substring(DataStorage.ProjectInfo.BaseFolder.From.Length);
-                                        CP.CopySingleFile(_file, ToPath);
-                                        FilesToUnload.Add(ToPath);
+                                        var FilesWithChanges = DataStorage.FilesWithChanges;
+                                        var FilesToUnload = new List<string>();
+
+                                        foreach (var _file in FilesWithChanges)
+                                        {
+                                            var ToPath = DataStorage.ProjectInfo.BaseFolder.To + _file.Substring(DataStorage.ProjectInfo.BaseFolder.From.Length);
+                                            CP.CopySingleFile(_file, ToPath);
+                                            FilesToUnload.Add(ToPath);
+                                        }
+                                        UAX.UnloadAllXref(FilesToUnload, AutomaticBool);
+
+
                                     }
-                                    UAX.UnloadAllXref(FilesToUnload, AutomaticBool);
-
-
-                                }
-                                else
-                                {
-                                    CP.DirectoryCopy(Folder.From, Folder.To, false, ".dwg");
-
-                                    UAX.UnloadAllXref(Directory.GetFiles(Folder.To).ToList(), AutomaticBool);
-                                }
-                            }
-
-                            //Lag ny IFC for eksport
-                            CP.TomIFCCopy(DataStorage.ProjectInfo.TomIFC, Export.Name);
-
-                            //Last ned single filer
-                            foreach (var File in DataStorage.ProjectInfo.Files)
-                            {
-                                CP.CopySingleFile(File.From, File.To);
-                            }
-
-                            //--Åpne starttegning og sett som aktiv
-                            OAC.OpenDrawing(Discipline.StartFile.To);
-                            Application.DocumentManager.MdiActiveDocument = OAC.ReturnActivateDrawing(Discipline.StartFile.To);
-
-                            //--Kjør eksport
-                            DataStorage.app.ActiveDocument.SendCommand("_.-MAGIIFCEXPORT " + Export.Name + "\n");
-
-                            //--Lukk tegning
-                            Document doc = Application.DocumentManager.MdiActiveDocument;
-                            doc.CloseAndDiscard();
-
-                            //--Last opp IFC
-                            var IfcFromPath = Path.GetDirectoryName(DataStorage.ProjectInfo.TomIFC.To) + "\\" + Export.Name + ".ifc";
-                            var IfcToPath = DataStorage.ProjectInfo.TomIFC.Export + "\\" + Export.IFC + ".ifc";
-
-                            bool fileCopied = true;
-                            int Attempts = 0;
-
-                            while (fileCopied)
-                            {
-                                Attempts++;
-                                try
-                                {
-                                    CP.CopySingleFile(IfcFromPath, IfcToPath);
-                                    fileCopied = false;
-                                }
-                                catch (System.Exception e)
-                                {
-                                    if (Attempts == 10)
+                                    else
                                     {
-                                        Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("Unable to upload IFC-file, error: " + e.Message);
-                                        break;
+                                        CP.DirectoryCopy(Folder.From, Folder.To, false, ".dwg");
+
+                                        UAX.UnloadAllXref(Directory.GetFiles(Folder.To).ToList(), AutomaticBool);
                                     }
-                                    Thread.Sleep(2000);
-                                    fileCopied = true;
+                                }
+
+                                //Lag ny IFC for eksport
+                                CP.TomIFCCopy(DataStorage.ProjectInfo.TomIFC, Export.Name);
+
+                                //Last ned single filer
+                                foreach (var File in DataStorage.ProjectInfo.Files)
+                                {
+                                    CP.CopySingleFile(File.From, File.To);
+                                }
+
+                                //--Åpne starttegning og sett som aktiv
+                                OAC.OpenDrawing(Discipline.StartFile.To);
+                                Application.DocumentManager.MdiActiveDocument = OAC.ReturnActivateDrawing(Discipline.StartFile.To);
+
+                                //--Kjør eksport
+                                DataStorage.app.ActiveDocument.SendCommand("_.-MAGIIFCEXPORT " + Export.Name + "\n");
+
+                                //--Lukk tegning
+                                Document doc = Application.DocumentManager.MdiActiveDocument;
+                                doc.CloseAndDiscard();
+
+                                //--Last opp IFC
+                                var IfcFromPath = Path.GetDirectoryName(DataStorage.ProjectInfo.TomIFC.To) + "\\" + Export.Name + ".ifc";
+                                var IfcToPath = DataStorage.ProjectInfo.TomIFC.Export + "\\" + Export.IFC + ".ifc";
+
+                                bool fileCopied = true;
+                                int Attempts = 0;
+
+                                while (fileCopied)
+                                {
+                                    Attempts++;
+                                    try
+                                    {
+                                        CP.CopySingleFile(IfcFromPath, IfcToPath);
+                                        fileCopied = false;
+                                    }
+                                    catch (System.Exception e)
+                                    {
+                                        if (Attempts == 10)
+                                        {
+                                            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("Unable to upload IFC-file, error: " + e.Message);
+                                            break;
+                                        }
+                                        Thread.Sleep(2000);
+                                        fileCopied = true;
+                                    }
                                 }
                             }
                         }
                     }
                 }
+                if (AutomaticBool)
+                {
+                    SleepBeforeReset();
+                }
             }
-            if (AutomaticBool)
+            catch (System.Exception e)
             {
-                SleepBeforeReset();
-            }
+                System.Windows.MessageBox.Show(e.Message);
+                throw;
+            }            
         }
 
 
